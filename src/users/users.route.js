94c864eccf716b48afc27989d2a5429aab59124a
Gustavo Martins <gugumartin77@gmail.com>
2022-05-09 21:10:06 -0300

    Token request in get all users

--- src/users/users.route.js
+++ src/users/users.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userController = require('./users.controller');
+const loginMiddleware = require('../login/login.middleware');
 
 router.post('/create', userController.createUserController);
-router.get('/', userController.findAllUserController);
+router.get('/', loginMiddleware, userController.findAllUserController);
 
 module.exports = router;
